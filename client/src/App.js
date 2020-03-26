b85e102588221f88a0d131e99f03ec8d02f38957
Katleen Ludger <kat@Katleens-MacBook-Pro.local>
2020-03-26 11:58:49 -0400

    imported styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,21 @@
-import React, { Fragment } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './components/layout/Navbar';
-import Home from './components/pages/Home';
-import About from './components/pages/About';
-import Register from './components/auth/Register';
-import Login from './components/auth/Login';
-import Alerts from './components/layout/Alerts';
-import PrivateRoute from './components/routing/PrivateRoute';
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import Alerts from "./components/layout/Alerts";
+import PrivateRoute from "./components/routing/PrivateRoute";
 
-import ContactState from './context/contact/ContactState';
-import AuthState from './context/auth/AuthState';
-import AlertState from './context/alert/AlertState';
-import setAuthToken from './utils/setAuthToken';
+import ContactState from "./context/contact/ContactState";
+import AuthState from "./context/auth/AuthState";
+import AlertState from "./context/alert/AlertState";
+import setAuthToken from "./utils/setAuthToken";
 
 import { ReactComponent as WelcomeImg } from "./assets/welcome-img.svg";
 
-import './App.css';
+import "./App.css";
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -29,22 +29,23 @@ const App = () => {
           <Router>
             <Fragment>
               <Navbar />
-              <div className='container'>
-                <Alerts />
-                <Switch>
-                  <PrivateRoute exact path='/' component={Home} />
-                  <div className="guest-container">
-                    <div className="guest-container__img">
-                        <WelcomeImg className="welcome-img"/>
+              <div>
+                <Alerts/>
+                <div className="container">
+                  <Switch>
+                    <PrivateRoute exact path="/" component={Home} />
+                    <div className="guest-container">
+                      <div className="guest-container__img">
+                        <WelcomeImg className="welcome-img" />
+                      </div>
+                      <div className="guest-container__routes">
+                        <Route exact path="/about" component={About} />
+                        <Route exact path="/register" component={Register} />
+                        <Route exact path="/login" component={Login} />
+                      </div>
                     </div>
-                    <div className="guest-container__routes">
-                      <Route exact path='/about' component={About} />
-                      <Route exact path='/register' component={Register} />
-                      <Route exact path='/login' component={Login} />
-                    </div>
-                  </div>
-
-                </Switch>
+                  </Switch>
+                </div>
               </div>
             </Fragment>
           </Router>
--- client/src/components/layout/Alerts.js
+++ client/src/components/layout/Alerts.js
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react';
 import AlertContext from '../../context/alert/alertContext';
 
+import '../../App.css'
+
 const Alert = () => {
   const alertContext = useContext(AlertContext);
   return (
